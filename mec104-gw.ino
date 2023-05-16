70f77eb25d98c59ecc653c30c03472f07722a5d4
Yang <Lyuxyang@gmail.com>
2023-05-16 11:51:44 +0800

    添加了hid库，rotation功能更新 网站说还需要hoodloader库支持，但我不会（ 如果不能成功的话就改了这个功能吧

--- mec104-gw.ino
+++ mec104-gw.ino
@@ -1,3 +1,10 @@
+#include <HID-Project.h>
+#include <HoodLoader.h>
+const int rotation = A1; //旋钮在模拟引脚A1
+int val = 0; // Variable to read value from potentiometer, starts at 0
+int oldVolume = 0; // Used to compare volume levels
+int currentVolume = 0; // Used to compare volume levels
+
 const int micPin = A0; // 麦克风传感器连接到模拟引脚 A0
 const int sleepTime = 10000; // 休眠时间（毫秒）
 unsigned long lastSoundTime = 0; // 上次检测到声音的时间
@@ -6,11 +13,13 @@ int wakeUpPin = 2;//看看能不能用旋钮或者其他开关什么的实现
 
 
 void setup() {
+  Serial.begin(9600); // This will allow you to read the current value of the dial
   pinMode(micPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  rotation();
   int micValue = analogRead(micPin); // 读取麦克风传感器的值
   if (micValue > 10) { // 如果检测到声音
     lastSoundTime = millis(); // 更新上次检测到声音的时间
@@ -34,35 +43,27 @@ void wakeUp() {
   sleep_disable(); // 禁用休眠功能
   detachInterrupt(0); // 取消中断配置
 }
-// #include
 
-// int potpin = 0; // Assign analog pin to potentiometer
-// int val = 0; // Variable to read value from potentiometer, starts at 0
-// int oldVolume = 0; // Used to compare volume levels
-// int currentVolume = 0; // Used to compare volume levels
+void rotation(){
+val = analogRead(rotation); // Reads potentiometer value (between 0 and 1023)
+val = map(val, 0, 1023, 0, 50); // Scale value to volume (value between 0 and 50)
+Serial.print(val); // Print dial/volume position
+Serial.println(); //
 
-// void setup() {
-// Serial.begin(9600); // This will allow you to read the current value of the dial
-// }
+if (val != oldVolume) {
+if(val > oldVolume){
+  //delay(100);
+Consumer.write(MEDIA_VOLUME_UP);
+currentVolume = currentVolume + 2;
+oldVolume = val;
+}
+else{
+//delay(100);
+Consumer.write(MEDIA_VOLUME_DOWN);
+currentVolume = currentVolume - 2;
+oldVolume = val;
+}
+}
+}
 
-// void loop() {
-// val = analogRead(potpin); // Reads potentiometer value (between 0 and 1023)
-// val = map(val, 0, 1023, 0, 50); // Scale value to volume (value between 0 and 50)
-// Serial.print(val); // Print dial/volume position
-// Serial.println(); //
-
-// if (val != oldVolume) {
-// if(val > oldVolume){
-// //delay(100);
-// Consumer.write(MEDIA_VOLUME_UP);
-// currentVolume = currentVolume + 2;
-// oldVolume = val;
-// }
-// else{
-// //delay(100);
-// Consumer.write(MEDIA_VOLUME_DOWN);
-// currentVolume = currentVolume – 2;
-// oldVolume = val;
-// }
-// }
 // }https://www.deviceplus.com/arduino/usb-volume-control-with-arduino/
